960ab1e27d0ffe58650578db8aadd3b9c8f6e2a3
rameez.shikalgar <Rdssdr123#>
2019-08-14 14:55:01 +0400

    Clean up

--- vision-automation/automation-util/src/main/java/com/emaratech/util/AutomationUtil.java
+++
@@ -1,9 +0,0 @@
-package com.emaratech.util;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class AutomationUtil {
-	public static void main(String...strings) throws Exception {
-		
-	}
-}
